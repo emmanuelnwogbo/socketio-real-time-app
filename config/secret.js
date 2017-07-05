1578e6b5f2f6b7c2cd8ce9104f6fe2b4ff511f52
Emmanuel Nwogbo <emmnlchuck@gmail.com>
2017-07-05 18:09:07 +0000

    third deploy

--- config/secret.js
+++ config/secret.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/twitterclonedb' || 'mongodb://heroku_vx0t3bs3:22c79d26f924h3obijfpdijn8j@ds011308.mlab.com:11308/heroku_vx0t3bs3',
+  database: 'mongodb://heroku_vx0t3bs3:22c79d26f924h3obijfpdijn8j@ds011308.mlab.com:11308/heroku_vx0t3bs3',
   secret: "hahahaha88888"
 };
